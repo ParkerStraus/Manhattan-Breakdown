e0e2bb6e9520d24cf383af83e64478b0d0d5a2ed
pestydesperado248 <58351647+pestydesperado248@users.noreply.github.com>
2024-03-23 22:19:56 +0000

    Room Creation

---
+++ Assets/Scripts/Multiplayer/CreateRooms.cs
@@ -0,0 +1,33 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using Photon.Realtime;
+
+public class CreateRooms : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private TMP_Text _roomName;
+
+    public void OnClick_CreateRoom()
+    {
+        if (!PhotonNetwork.IsConnected)
+        {
+            return;
+        }
+        RoomOptions option = new RoomOptions();
+        option.MaxPlayers = 4;
+        PhotonNetwork.JoinOrCreateRoom("basic", option, TypedLobby.Default);
+    }
+
+    public override void OnCreatedRoom()
+    {
+        print("Created room successfully");
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        print("room creation failed "+ message);
+        base.OnCreateRoomFailed(returnCode, message);
+    }
+}
